1981dddfaf80154561d4af508f91ac132241e300
Jared Hansen <jvhansen08@gmail.com>
2023-02-09 10:40:22 -0700

    adds prototypes

---
+++ Prototypes/DjangoReactPrototype/frontend/src/Types.tsx
@@ -0,0 +1,10 @@
+export type Car = {
+    vin: string,
+    make: string,
+    model: string,
+    edition: string,
+    year: number,
+    color: string,
+    mileage: number,
+    titleType: string,
+}
