ca52ab4453d9bf4049905df9ddafeb87da1f8e8d
Chase Miller <a02283751@aggies.usu.edu>
2023-02-10 15:48:14 -0700

    update models and start implement api

--- Prototypes/DjangoPrototype/autoshop/models.py
+++ Prototypes/DjangoPrototype/autoshop/models.py
@@ -4,15 +4,38 @@ from django.db import models
 from django.db import models
 
 
-class Car(models.Model):
-    car_type = models.CharField(max_length=200)    
-    car_color = models.CharField(max_length=200)
-    def __str__(self):
-        return self.car_type
+class Vehicle(models.Model):
+    name = models.CharField(max_length=200)
+    vim = models.IntegerField()
+    location = models.CharField(max_length=200)
+    isPurchased = models.BooleanField()
+    isPending = models.BooleanField()
+    reservedDays = models.JSONField()
+    vehicleType = models.CharField(max_length=20)
+    isInsured = models.BooleanField()
+    isLoadJacked = models.BooleanField()
+    dateCheckedOut = models.DateTimeField()
+    dateCheckedIn = models.DateTimeField()
+    image = models.TextField()  # string to the url of the image
+    # automatically assigns an id to each instance
 
-class Customer(models.Model):
-    customer_name = models.CharField(max_length=200)
-    birth_date = models.DateTimeField('date of birth')
     def __str__(self):
-        return self.customer_name
+        return self.name
 
+
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    birthDate = models.DateTimeField('date of birth')
+    username = models.CharField(max_length=12)
+    password = models.CharField(max_length=20)
+    # automatically assigns an id to each instance
+    balance = models.FloatField()
+    accessLevel = models.CharField(max_length=20)
+    email = models.EmailField(max_length=49)
+    phone = models.IntegerField()
+    needHelp = models.BooleanField()
+    ethicsViolation = models.TextField()
+    location = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.username
--- Prototypes/DjangoPrototype/autoshop/urls.py
+++ Prototypes/DjangoPrototype/autoshop/urls.py
@@ -10,4 +10,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('getUserInfo', views.getUserInfoApi, name='getUserInfoApi'),
+    # http://${location.host}/autoshop/getUserInfo?username=${username}
+    path('getVehicleInfo', views.getVehicleInfoApi, name='getVehicleInfoApi'),
+    # http://${location.host}/autoshop/getVehicleInfo?vim=${vim}
 ]
