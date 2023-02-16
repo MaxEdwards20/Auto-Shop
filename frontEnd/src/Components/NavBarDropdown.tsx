687eba41a96b4d26913e96c2e7e65fb8189f4764
medwards <maxedwards20@gmail.com>
2023-02-16 12:44:08 -0700

    added conditional rendering on the NavBar based off the context state.

--- frontEnd/src/Components/NavBarDropdown.tsx
+++ frontEnd/src/Components/NavBarDropdown.tsx
@@ -4,52 +4,24 @@ import { AuthContext } from "../context/AuthContext";
 import { useContext } from "react";
 
 export default function NavBarDropdown(props: any) {
-  const { isAuthenticated, userType, login } = useContext(AuthContext);
-  login("admin"); // for testing the different views
+  const { isAuthenticated } = useContext(AuthContext);
   if (!isAuthenticated) {
     return (
       <NavDropdown title="Account" id="basic-nav-dropdown">
         <NavDropdown.Item href="/account/login">Log In</NavDropdown.Item>
       </NavDropdown>
     );
-  }
-  // we know they are authenticated here
-  if (userType == "admin") {
+  } else {
     return (
       <NavDropdown title="Account" id="basic-nav-dropdown">
         <NavDropdown.Item href="/account">Account Info</NavDropdown.Item>
         <NavDropdown.Item href="/reservation">My Reservations</NavDropdown.Item>
-        <NavDropdown.Item href="/account/vehicles">
-          Reserved Vehicles
+        <NavDropdown.Item href="/account/balance">
+          Deposit Funds
         </NavDropdown.Item>
-        <NavDropdown.Item href="/payemployees">Pay Employees</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="/account/login">Log Out</NavDropdown.Item>
-      </NavDropdown>
-    );
-  } else if (userType == "employee") {
-    return (
-      <NavDropdown title="Account" id="basic-nav-dropdown">
-        <NavDropdown.Item href="/account">Account Info</NavDropdown.Item>
-        <NavDropdown.Item href="/reservation">My Reservations</NavDropdown.Item>
-        <NavDropdown.Item href="/account/vehicles">
-          Reserved Vehicles
-        </NavDropdown.Item>
-        <NavDropdown.Item href="/loghours">Log Hours</NavDropdown.Item>
         <NavDropdown.Divider />
         <NavDropdown.Item href="/account/login">Log Out</NavDropdown.Item>
       </NavDropdown>
     );
   }
-  return (
-    <NavDropdown title="Account" id="basic-nav-dropdown">
-      <NavDropdown.Item href="/account">Account Info</NavDropdown.Item>
-      <NavDropdown.Item href="/reservation">My Reservations</NavDropdown.Item>
-      <NavDropdown.Item href="/account/vehicles">
-        Reserved Vehicles
-      </NavDropdown.Item>
-      <NavDropdown.Divider />
-      <NavDropdown.Item href="/account/login">Log Out</NavDropdown.Item>
-    </NavDropdown>
-  );
 }
