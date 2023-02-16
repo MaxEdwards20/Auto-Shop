d41cbd1865f67bb63020125390619ea7ba2784a2
medwards <maxedwards20@gmail.com>
2023-02-16 11:47:56 -0700

    Added context to the requests to for user type and updated dropdown on the NavBar to match better

--- frontEnd/src/App.tsx
+++ frontEnd/src/App.tsx
@@ -2,14 +2,18 @@ import { BrowserRouter } from "react-router-dom";
 import Router from "./Components/Router";
 import NavBar from "./Components/Navbar";
 import { useState } from "react";
-
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
+import { AuthProvider } from "./context/AuthContext";
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  useContext(AuthContext);
   return (
-    <BrowserRouter>
-      <NavBar props={isAuthenticated} />
-      <Router></Router>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <NavBar />
+        <Router></Router>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
--- frontEnd/src/Components/NavBarDropdown.tsx
+++ frontEnd/src/Components/NavBarDropdown.tsx
@@ -1,33 +1,55 @@
+import context from "react-bootstrap/esm/AccordionContext";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import { AuthContext } from "../context/AuthContext";
+import { useContext } from "react";
 
 export default function NavBarDropdown(props: any) {
-  const isAuthenticated = props.isAuthenticated;
+  const { isAuthenticated, userType, login } = useContext(AuthContext);
+  login("admin"); // for testing the different views
+  if (!isAuthenticated) {
+    return (
+      <NavDropdown title="Account" id="basic-nav-dropdown">
+        <NavDropdown.Item href="/account/login">Log In</NavDropdown.Item>
+      </NavDropdown>
+    );
+  }
+  // we know they are authenticated here
+  if (userType == "admin") {
+    return (
+      <NavDropdown title="Account" id="basic-nav-dropdown">
+        <NavDropdown.Item href="/account">Account Info</NavDropdown.Item>
+        <NavDropdown.Item href="/reservation">My Reservations</NavDropdown.Item>
+        <NavDropdown.Item href="/account/vehicles">
+          Reserved Vehicles
+        </NavDropdown.Item>
+        <NavDropdown.Item href="/payemployees">Pay Employees</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="/account/login">Log Out</NavDropdown.Item>
+      </NavDropdown>
+    );
+  } else if (userType == "employee") {
+    return (
+      <NavDropdown title="Account" id="basic-nav-dropdown">
+        <NavDropdown.Item href="/account">Account Info</NavDropdown.Item>
+        <NavDropdown.Item href="/reservation">My Reservations</NavDropdown.Item>
+        <NavDropdown.Item href="/account/vehicles">
+          Reserved Vehicles
+        </NavDropdown.Item>
+        <NavDropdown.Item href="/loghours">Log Hours</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="/account/login">Log Out</NavDropdown.Item>
+      </NavDropdown>
+    );
+  }
   return (
-    <div>
-      {isAuthenticated ? (
-        <NavDropdown title="Account" id="basic-nav-dropdown">
-          <NavDropdown.Item href="/account">Account Info</NavDropdown.Item>
-          <NavDropdown.Item href="/reservation">
-            My Reservations
-          </NavDropdown.Item>
-          <NavDropdown.Item href="/account/vehicles">
-            Reserved Vehicles
-          </NavDropdown.Item>
-          {props.isManager ? (
-            <NavDropdown.Item href="/account/pay">
-              Pay Employees
-            </NavDropdown.Item>
-          ) : (
-            <NavDropdown.Item href="/account/hours">Log Hours</NavDropdown.Item>
-          )}
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="/account/login">Log Out</NavDropdown.Item>
-        </NavDropdown>
-      ) : (
-        <NavDropdown title="Account" id="basic-nav-dropdown">
-          <NavDropdown.Item href="/account/login">Log In</NavDropdown.Item>
-        </NavDropdown>
-      )}
-    </div>
+    <NavDropdown title="Account" id="basic-nav-dropdown">
+      <NavDropdown.Item href="/account">Account Info</NavDropdown.Item>
+      <NavDropdown.Item href="/reservation">My Reservations</NavDropdown.Item>
+      <NavDropdown.Item href="/account/vehicles">
+        Reserved Vehicles
+      </NavDropdown.Item>
+      <NavDropdown.Divider />
+      <NavDropdown.Item href="/account/login">Log Out</NavDropdown.Item>
+    </NavDropdown>
   );
 }
