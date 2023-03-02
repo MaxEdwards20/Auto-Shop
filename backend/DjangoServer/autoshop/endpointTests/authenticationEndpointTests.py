34f672bb1427d21e9ea72272f84cf7c42d82e404
Chase Miller <a02283751@aggies.usu.edu>
2023-03-02 11:25:12 -0700

    endpoint tests

---
+++ backend/DjangoServer/autoshop/endpointTests/authenticationEndpointTests.py
@@ -0,0 +1,29 @@
+from django.test import TestCase, Client
+import uuid
+from django.contrib.auth.models import User as Person
+import json
+# Create your tests here.
+
+class TestAuthentication(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.email = f'test@gmail.com{uuid.uuid4()}'
+        response = self.client.post('http://localhost:8000/user', data={'name': 'test', 'email': self.email,
+                                                                        'password': 'testpass', 'phone': '2222222'})
+        self.assertEqual(response.status_code, 200)
+
+    def testAuthentication(self):
+        response = self.client.post('http://localhost:8000/user/login', data={'email': self.email, "password": "testpass"})
+        self.assertEqual(response.status_code, 200)
+
+    def testUnauthenticatedUser(self):
+        response = self.client.post('http://localhost:8000/user/login',
+                                    data={'email': 'INVALID', "password": "INVALID"})
+        self.assertEqual(response.status_code, 401)
+        response = self.client.post('http://localhost:8000/user/login',
+                                    data={'email': self.email, "password": "INVALID"})
+        self.assertEqual(response.status_code, 401)
+
+        response = self.client.post('http://localhost:8000/user/login',
+                                    data={'email': 'INVALID', "password": "testpass"})
+        self.assertEqual(response.status_code, 401)
--- backend/DjangoServer/autoshop/tests.py
+++ backend/DjangoServer/autoshop/tests.py
@@ -1,25 +1,3 @@
-from django.test import TestCase, Client
-from django.contrib.auth.models import User as Person
-import json
-# Create your tests here.
+from .endpointTests.authenticationEndpointTests import TestAuthentication
 
-class TestViews(TestCase):
-    def setUp(self):
-        self.client = Client()
-        self.user = Person.objects.create_user(username="testuser", password="testpass")
-
-    def testAuthentication(self):
-        response = self.client.post('http://localhost:8000/user/login', data={'email': 'testuser', "password": "testpass"})
-        self.assertEqual(response.status_code, 200)
-
-    def testUnauthenticatedUser(self):
-        response = self.client.post('http://localhost:8000/user/login',
-                                    data={'email': 'INVALID', "password": "INVALID"})
-        self.assertEqual(response.status_code, 401)
-        response = self.client.post('http://localhost:8000/user/login',
-                                    data={'email': 'testuser', "password": "INVALID"})
-        self.assertEqual(response.status_code, 401)
-
-        response = self.client.post('http://localhost:8000/user/login',
-                                    data={'email': 'INVALID', "password": "testpass"})
-        self.assertEqual(response.status_code, 401)
+userEndpoints = TestAuthentication()
